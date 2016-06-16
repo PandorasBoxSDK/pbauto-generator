abe48bd1a086dc87e6bb55a203563e1f9ce7a282
Dennis Kuypers <kuypers@coolux.de>
2016-06-16 08:55:48 +0200

    eplicit byte array

--- source/csharp/PandorasBox.cs
+++ source/csharp/PandorasBox.cs
@@ -246,7 +246,7 @@ namespace PandorasBox
                 if(header[0] != 0x50 || header[1] != 0x42 || header[2] != 0x41 || header[3] != 0x55 || header.PBAutoChecksum() != header[16])
                 {
                     // Not a PB Header or checksum fail
-                    return new ByteUtil(new []{ 255, 255, 0, 0, 0, 7});
+                    return new ByteUtil(new byte[]{ 255, 255, 0, 0, 0, 7});
                 }
 
                 int message_length = header.GetInt16(9);
